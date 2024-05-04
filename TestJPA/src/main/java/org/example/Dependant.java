654c6a0159b4739bc99bcad8a01d2ba505ca5bd9
duybui1311 <congduy980@gmail.com>
2024-05-04 23:17:18 +0700

    added dependant application and controller

---
+++ TestJPA/src/main/java/org/example/Dependant.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Dependant extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Dependant.fxml"));
+
+        // Set up the stage and scene
+        primaryStage.setTitle("FXML Example");
+        primaryStage.setScene(new Scene(root, 819, 551));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ TestJPA/src/main/java/org/example/DependantController.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DependantController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
