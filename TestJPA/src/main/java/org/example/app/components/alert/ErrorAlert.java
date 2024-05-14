1c0548504936a9ccabdc548b1c6273054fc9c98a
vinhnt2914 <113036284+vinhnt2914@users.noreply.github.com>
2024-05-14 15:33:15 +0700

    implement base for alerts

---
+++ TestJPA/src/main/java/org/example/app/components/alert/ErrorAlert.java
@@ -0,0 +1,12 @@
+package org.example.app.components.alert;
+
+public class ErrorAlert extends CustomAlert{
+    public ErrorAlert(String message) {
+        super(message);
+    }
+
+    @Override
+    void setUpAlert() {
+
+    }
+}
---
+++ TestJPA/src/main/java/org/example/app/components/alert/WarningAlert.java
@@ -0,0 +1,12 @@
+package org.example.app.components.alert;
+
+public class WarningAlert extends CustomAlert{
+    public WarningAlert(String message) {
+        super(message);
+    }
+
+    @Override
+    void setUpAlert() {
+
+    }
+}
