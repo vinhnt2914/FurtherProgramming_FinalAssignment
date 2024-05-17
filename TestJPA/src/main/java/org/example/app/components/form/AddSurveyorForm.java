33c69b62128bf60d3cdf257e10ce6586476e67f2
duybui1311 <congduy980@gmail.com>
2024-05-17 22:03:14 +0700

    added choice to choose manager when adding surveyor

--- TestJPA/src/main/java/org/example/app/components/form/AddSurveyorForm.java
+++ TestJPA/src/main/java/org/example/app/components/form/AddSurveyorForm.java
@@ -1,34 +1,27 @@
 package org.example.app.components.form;
 
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.example.app.components.alert.ErrorAlert;
-import org.example.app.components.table.SelectManagerTable;
-import org.example.app.components.table.SelectPolicyOwnerTable;
 import org.example.app.controllers.RefreshableController;
-import org.example.global.CustomerQueryType;
 import org.example.global.GlobalVariable;
-import org.example.global.ProviderQueryType;
 import org.example.global.Role;
 import org.example.model.customer.PolicyHolder;
 import org.example.model.customer.PolicyOwner;
 import org.example.model.provider.InsuranceManager;
 import org.example.model.provider.InsuranceSurveyor;
-import org.example.repository.impl.CustomerRepository;
 import org.example.repository.impl.ProviderRepository;
-import org.example.service.CustomerService;
 import org.example.service.ProviderService;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class AddSurveyorForm extends BorderPane{
@@ -45,18 +38,14 @@ public class AddSurveyorForm extends BorderPane{
     @FXML
     private TextField passwordField;
     @FXML
-    private VBox managerContainer;
-    @FXML
-    private Button selectManagerButton;
-    @FXML
-    private Label managerLabel;
+    private ComboBox<InsuranceManager> managerComboBox;
     @FXML
     private Button saveButton;
     @FXML
     private Button cancelButton;
+
     private Stage stage;
     private RefreshableController controller;
-    private InsuranceManager manager;
 
     public AddSurveyorForm(RefreshableController controller) {
         this.controller = controller;
@@ -75,19 +64,22 @@ public class AddSurveyorForm extends BorderPane{
         }
 
         setUpForm();
+        loadManagers();
     }
 
     private void setUpForm() {
-        this.selectManagerButton.setOnAction(this::openSelectManager);
         this.saveButton.setOnAction(this::addSurveyor);
         this.cancelButton.setOnAction(this::handleCancel);
     }
 
-    private void openSelectManager(ActionEvent actionEvent) {
-        new SelectManagerTable(ProviderQueryType.QueryType.GET_ALL, this);
+    private void loadManagers() {
+        ProviderRepository repository = new ProviderRepository();
+        List<InsuranceManager> managers = repository.getAllManager();
+        ObservableList<InsuranceManager> managerList = FXCollections.observableArrayList(managers);
+        managerComboBox.setItems(managerList);
     }
 
-    void addSurveyor(ActionEvent actionEvent) {
+    private void addSurveyor(ActionEvent actionEvent) {
         if (validateInput()) {
             ProviderRepository repository = new ProviderRepository();
             ProviderService service = new ProviderService();
@@ -99,14 +91,14 @@ public class AddSurveyorForm extends BorderPane{
                     .email(emailField.getText())
                     .phone(phoneField.getText())
                     .password(passwordField.getText())
-                    .manager(manager)
+                    .manager(managerComboBox.getValue())
                     .build();
 
             repository.add(surveyor);
+
+            controller.refresh();
             repository.close();
 
-            // Dont put refresh after close(), or controller will be null
-            controller.refresh();
             close();
         }
     }
@@ -114,7 +106,7 @@ public class AddSurveyorForm extends BorderPane{
     private boolean validateInput() {
         if (isFieldEmpty(nameField) || isFieldEmpty(usernameField) || isFieldEmpty(addressField) ||
                 isFieldEmpty(emailField) || isFieldEmpty(phoneField) || isFieldEmpty(passwordField) ||
-                isPolicyOwnerNull()) {
+                isManagerNull()) {
             new ErrorAlert("All fields must be filled out.");
             return false;
         }
@@ -131,26 +123,21 @@ public class AddSurveyorForm extends BorderPane{
         return field.getText() == null || field.getText().trim().isEmpty();
     }
 
-
     private boolean isValidEmail(String email) {
         String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
         Pattern pattern = Pattern.compile(emailRegex);
         return pattern.matcher(email).matches();
     }
 
-    private boolean isPolicyOwnerNull() {
-        return manager == null;
+    private boolean isManagerNull() {
+        return managerComboBox.getValue() == null;
     }
 
     private void handleCancel(ActionEvent actionEvent) {
         close();
     }
+
     private void close() {
         stage.close();
-    }
-
-    public void setManager(InsuranceManager manager) {
-        this.manager = manager;
-        managerLabel.setText(manager.getId() + " - " + manager.getFullName());
     }
 }
