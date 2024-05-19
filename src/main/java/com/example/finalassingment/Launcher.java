93fcb96090b31bd2426c033992f4edc13987389c
vinhnt2914 <113036284+vinhnt2914@users.noreply.github.com>
2024-05-19 13:16:27 +0700

    Create executable jar

---
+++ src/main/java/com/example/finalassingment/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.finalassingment;
+
+import java.sql.SQLException;
+
+public class Launcher {
+    public static void main(String[] args) throws SQLException {
+        Main.main(args);
+    }
+}
